52d56b686320d82984ea1e3cd81dd79c3fb31742
Morning Star <alexbestoso@gmail.com>
2025-03-24 08:24:27 -0400

    Getting her started

---
+++ NEW_CODE_BASE/main.cc
@@ -0,0 +1,6 @@
+#include "./main.h"
+
+int main(int argc, char *argv[]){
+	printf("Let us begin...\n");
+	exit(EXIT_SUCCESS);
+}
---
+++ NEW_CODE_BASE/main.h
@@ -0,0 +1,32 @@
+#include <GLES3/gl3.h>
+#include <EGL/egl.h>
+#include <GLFW/glfw3.h>
+
+#include <X11/Xlib.h>
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+#include "./src/glm/glm/glm.hpp"
+#include "./src/glm/glm/gtc/matrix_transform.hpp"
+#include "./src/glm/glm/gtc/type_ptr.hpp"
+
+#include <dlfcn.h>
+#include <memory>
+#include <stdio.h>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <math.h>
+#include <iostream>
+#include <dirent.h>
+#include <fcntl.h>
+#include <typeinfo>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void mouse_click_callback(GLFWwindow* window, int button, int action, int mod);
+void keyboard_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
